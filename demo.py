fcce207d7fd2d356d0f408e00f3e87ae653fd30a
Tony Storni <tony@noteworth.com>
2018-08-26 12:00:51 -0300

    PEP 8 issues

--- demo.py
+++ demo.py
@@ -22,12 +22,12 @@ warnings.filterwarnings('ignore')
 
 def main(yolo):
 
-   # Definition of the parameters
+    # Definition of the parameters
     max_cosine_distance = 0.3
     nn_budget = None
     nms_max_overlap = 1.0
     
-   # deep_sort 
+    # deep_sort
     model_filename = 'model_data/mars-small128.pb'
     encoder = gdet.create_box_encoder(model_filename,batch_size=1)
     
@@ -39,7 +39,7 @@ def main(yolo):
     video_capture = cv2.VideoCapture(0)
 
     if writeVideo_flag:
-    # Define the codec and create VideoWriter object
+        # Define the codec and create VideoWriter object
         w = int(video_capture.get(3))
         h = int(video_capture.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
@@ -50,13 +50,13 @@ def main(yolo):
     fps = 0.0
     while True:
         ret, frame = video_capture.read()  # frame shape 640*480*3
-        if ret != True:
-            break;
+        if ret:
+            break
         t1 = time.time()
 
         image = Image.fromarray(frame)
         boxs = yolo.detect_image(image)
-       # print("box_num",len(boxs))
+        # print("box_num",len(boxs))
         features = encoder(frame,boxs)
         
         # score to 1.0 here).
@@ -76,8 +76,8 @@ def main(yolo):
             if track.is_confirmed() and track.time_since_update >1 :
                 continue 
             bbox = track.to_tlbr()
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+            cv2.putText(frame, str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 5e-3 * 200, (0, 255, 0), 2)
 
         for det in detections:
             bbox = det.to_tlbr()
@@ -91,12 +91,12 @@ def main(yolo):
             frame_index = frame_index + 1
             list_file.write(str(frame_index)+' ')
             if len(boxs) != 0:
-                for i in range(0,len(boxs)):
+                for i in range(0, len(boxs)):
                     list_file.write(str(boxs[i][0]) + ' '+str(boxs[i][1]) + ' '+str(boxs[i][2]) + ' '+str(boxs[i][3]) + ' ')
             list_file.write('\n')
             
-        fps  = ( fps + (1./(time.time()-t1)) ) / 2
-        print("fps= %f"%(fps))
+        fps = (fps + (1./(time.time()-t1))) / 2
+        print("fps= %f"%fps)
         
         # Press Q to stop!
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -107,6 +107,7 @@ def main(yolo):
         out.release()
         list_file.close()
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(YOLO())
